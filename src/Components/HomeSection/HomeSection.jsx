2e5cbde509d0c5272addd4bfe4a989aefa8dbf11
Nergiz Seferli <nergizsafarli60@gmail.com>
2025-03-30 23:40:24 +0400

    home section created

--- src/Components/HomeSection/HomeSection.jsx
+++ src/Components/HomeSection/HomeSection.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./HomeSection.css";
+import { useNavigate } from "react-router-dom";
 
 function HomeSection() {
+  const navigate=useNavigate()
   return (
-    <div>HomeSection</div>
-  )
+    <div className="homeSection-container">
+      <div className="studio">A Film By Goodmov Studio</div>
+      <div className="home-inf">
+        <h2>48 Hours with Z</h2>
+        <p>"Outrun of The Z, Outlast The Night"</p>
+        <span>
+          BILL SMITH | JENNIFER SHANCEZ | SYLVESTER STULHEN | HANK GRILLO |
+          MERIAM BALIN
+        </span>
+        <button onClick={()=>navigate("/movie")} className="discover-more">Discover More</button>
+      </div>
+    </div>
+  );
 }
 
-export default HomeSection
+export default HomeSection;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../Components/Navbar/Navbar'
+import HomeSection from '../Components/HomeSection/HomeSection'
 
 function HomePage() {
   return (
     <div>
         <Navbar/>
+        <HomeSection/>
     </div>
   )
 }
