d79a718d3c01d26a4546b04314256451c9153227
Nergiz Seferli <nergizsafarli60@gmail.com>
2025-03-30 22:00:27 +0400

    detail popup added

--- src/Components/Movie/Movie.jsx
+++ src/Components/Movie/Movie.jsx
@@ -1,34 +1,137 @@
 
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { fetchMovies, fetchGenres, setSelectedGenre } from '../../redux/movieSlice';
+// import { Select, Input } from 'antd';
+// import { AudioOutlined, EyeOutlined, HeartOutlined } from '@ant-design/icons';
+// import "./Movie.css"
+
+// function Movie() {
+
+//   const { Search } = Input;
+//   const suffix = (
+//     <AudioOutlined
+//       style={{
+//         fontSize: 16,
+//         color: '#1677ff',
+//       }}
+//     />
+//   );
+
+//   const onSearch = (value, _e, info) =>
+//     console.log(info === null || info === void 0 ? void 0 : info.source, value);
+
+//   const dispatch = useDispatch();
+//   const { movies, page, totalPages, status, genres, selectedGenre } = useSelector((store) => store.movies);
+//    console.log(genres,"genres");
+//    console.log(movies,"movie");
+
+//   useEffect(() => {
+//     dispatch(fetchMovies(1)); // Filmləri çəkirik
+//     dispatch(fetchGenres()); // Janrları çəkirik
+//   }, [dispatch]);
+
+//   const handleChange = (genreId) => {
+//     dispatch(setSelectedGenre(genreId));
+//   };
+
+//   const getGenreNames = (genreIds) => {
+//     return genreIds
+//       .map((id) => genres.find((genre) => genre.id === id)?.name)
+//       .filter(Boolean)
+//       .join(", ");
+//   };
+
+//   const filteredMovies =
+//     selectedGenre === "All genres"
+//       ? movies
+//       : movies.filter((movie) => movie.genre_ids.includes(parseInt(selectedGenre)));
+
+//   const handleDetail = (movieId) => {
+//     console.log("Detail for movie ID:", movieId);
+//   };
+
+//   const handleLike = (movieId) => {
+//     console.log("Liked movie ID:", movieId);
+//   };
+
+//   return (
+//     <div className='movie-container'>
+//     <div className='functional-div'>
+//     <div className='filter-div'>
+//         <p>Filter by Genre</p>
+//         <Select
+//           value={selectedGenre}
+//           style={{ width: 200 }}
+//           onChange={handleChange}
+//           options={genres.map((genre) => ({
+//             value: genre.id,
+//             label: genre.name,
+//           }))}
+//         />
+//       </div>
+//       <div className='search-div'>
+//     <Search placeholder="input search text" onSearch={onSearch} style={{ width: 200 }} />
+//       </div>
+//     </div>
+
+//       <div className="movie-grid">
+//       {filteredMovies.length > 0 ? (
+//       filteredMovies.map((movie) => (
+//         <div key={movie.id} className="movie-card">
+//           <img
+//             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+//             alt={movie.title}
+//           />
+//           <div className="movie-overlay">
+//             <div className="icons">
+//               <EyeOutlined className="icon" onClick={() => handleDetail(movie.id)} />
+//               <HeartOutlined className="icon" onClick={() => handleLike(movie.id)} />
+//             </div>
+//           </div>
+//           <h3 className="movie-title">{movie.title}</h3>
+//           <p className="movie-genre">{getGenreNames(movie.genre_ids)}</p>
+//         </div>
+//       ))
+//     ) : (
+//       <p>Belə bir film yoxdur</p>
+//     )}
+//       </div>
+
+//       {status === "loading" && <p>Loading...</p>}
+//       {page <= totalPages && (
+//         <button onClick={() => dispatch(fetchMovies(page))} disabled={status === "loading"}>
+//           {status === "loading" ? "Loading..." : "Load More"}
+//         </button>
+//       )}
+//     </div> 
+//   );
+// }
+
+// export default Movie;
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMovies, fetchGenres, setSelectedGenre } from '../../redux/movieSlice';
-import { Select, Input } from 'antd';
+import { Select, Input, Modal } from 'antd';
 import { AudioOutlined, EyeOutlined, HeartOutlined } from '@ant-design/icons';
-import "./Movie.css"
+import "./Movie.css";
 
 function Movie() {
 
   const { Search } = Input;
   const suffix = (
-    <AudioOutlined
-      style={{
-        fontSize: 16,
-        color: '#1677ff',
-      }}
-    />
+    <AudioOutlined style={{ fontSize: 16, color: '#1677ff' }} />
   );
 
-  const onSearch = (value, _e, info) =>
-    console.log(info === null || info === void 0 ? void 0 : info.source, value);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
   const dispatch = useDispatch();
   const { movies, page, totalPages, status, genres, selectedGenre } = useSelector((store) => store.movies);
-   console.log(genres,"genres");
-   console.log(movies,"movie");
 
   useEffect(() => {
-    dispatch(fetchMovies(1)); // Filmləri çəkirik
-    dispatch(fetchGenres()); // Janrları çəkirik
+    dispatch(fetchMovies(1));
+    dispatch(fetchGenres());
   }, [dispatch]);
 
   const handleChange = (genreId) => {
@@ -47,55 +150,61 @@ function Movie() {
       ? movies
       : movies.filter((movie) => movie.genre_ids.includes(parseInt(selectedGenre)));
 
-  const handleDetail = (movieId) => {
-    console.log("Detail for movie ID:", movieId);
+  const handleDetail = (movie) => {
+    setSelectedMovie(movie);
+    setIsModalOpen(true);
   };
 
   const handleLike = (movieId) => {
     console.log("Liked movie ID:", movieId);
   };
 
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedMovie(null);
+  };
+
   return (
     <div className='movie-container'>
-    <div className='functional-div'>
-    <div className='filter-div'>
-        <p>Filter by Genre</p>
-        <Select
-          value={selectedGenre}
-          style={{ width: 200 }}
-          onChange={handleChange}
-          options={genres.map((genre) => ({
-            value: genre.id,
-            label: genre.name,
-          }))}
-        />
+      <div className='functional-div'>
+        <div className='filter-div'>
+          <p>Filter by Genre</p>
+          <Select
+            value={selectedGenre}
+            style={{ width: 200 }}
+            onChange={handleChange}
+            options={genres.map((genre) => ({
+              value: genre.id,
+              label: genre.name,
+            }))}
+          />
+        </div>
+        <div className='search-div'>
+          <Search placeholder="input search text" onSearch={() => {}} style={{ width: 200 }} />
+        </div>
       </div>
-      <div className='search-div'>
-    <Search placeholder="input search text" onSearch={onSearch} style={{ width: 200 }} />
-      </div>
-    </div>
 
       <div className="movie-grid">
-      {filteredMovies.length > 0 ? (
-      filteredMovies.map((movie) => (
-        <div key={movie.id} className="movie-card">
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            alt={movie.title}
-          />
-          <div className="movie-overlay">
-            <div className="icons">
-              <EyeOutlined className="icon" onClick={() => handleDetail(movie.id)} />
-              <HeartOutlined className="icon" onClick={() => handleLike(movie.id)} />
+        {filteredMovies.length > 0 ? (
+          filteredMovies.map((movie) => (
+            <div key={movie.id} className="movie-card">
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <div className="movie-overlay">
+                <div className="icons">
+                  <EyeOutlined className="icon" onClick={() => handleDetail(movie)} />
+                  <HeartOutlined className="icon" onClick={() => handleLike(movie.id)} />
+                </div>
+              </div>
+              <h3 className="movie-title">{movie.title}</h3>
+              <p className="movie-genre">{getGenreNames(movie.genre_ids)}</p>
             </div>
-          </div>
-          <h3 className="movie-title">{movie.title}</h3>
-          <p className="movie-genre">{getGenreNames(movie.genre_ids)}</p>
-        </div>
-      ))
-    ) : (
-      <p>Belə bir film yoxdur</p>
-    )}
+          ))
+        ) : (
+          <p>Belə bir film yoxdur</p>
+        )}
       </div>
 
       {status === "loading" && <p>Loading...</p>}
@@ -104,8 +213,19 @@ function Movie() {
           {status === "loading" ? "Loading..." : "Load More"}
         </button>
       )}
-    </div> 
+
+      {selectedMovie && (
+        <Modal open={isModalOpen} onCancel={closeModal} footer={null}>
+          <img src={`https://image.tmdb.org/t/p/w500${selectedMovie.poster_path}`} alt={selectedMovie.title} style={{ width: "90%" ,height:"300px"}} />
+          <h2>{selectedMovie.title}</h2>
+          <p>{selectedMovie.overview}</p>
+          <p>Release Date: {selectedMovie.release_date}</p>
+          <p>Rating: {selectedMovie.vote_average}</p>
+        </Modal>
+      )}
+    </div>
   );
 }
 
 export default Movie;
+
